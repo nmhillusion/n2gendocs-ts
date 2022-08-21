5e624c906e60968b3ca991d4f1678550021e2fda
nmhillusion <nguyenminhhieu.geek@gmail.com>
2022-08-21 15:07:14 +0700

    ignore test_output

--- test/test-suits/test.template.ts
+++ test/test-suits/test.template.ts
@@ -4,6 +4,8 @@ import * as path from "path";
 import * as fs from "fs";
 
 export function fromTestTemplate(fileNameToTest: string) {
+  makeSureExistTestOutputFolder();
+
   return (testName: string) =>
     test(testName, () => {
       const docsOutput = new DocsGenerator(
@@ -25,4 +27,13 @@ export function fromTestTemplate(fileNameToTest: string) {
         util.text.stringToNormalLines(expectedOutput)
       );
     });
+}
+
+function makeSureExistTestOutputFolder() {
+  const testOutputPath = path.join(__dirname, `../test_output`);
+  if (!fs.existsSync(testOutputPath)) {
+    fs.mkdirSync(testOutputPath, {
+      recursive: true,
+    });
+  }
 }
